e41faa9cd6cb9d5a4236b06f26622a5423b8aed1
Larson Collier <collierlarson@gmail.com>
2022-11-01 02:18:31 -0400

    commit tree 1fcdf267cbc37c74433987d15df6ffac27767ca2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+//distanceFromHqInBlocks: Returns the number of blocks from Scuber's headquarters to the pickup location.
+const distanceFromHqInBlocks = (someValue) => {
+    let distance = Math.abs(someValue - 42)
+    return distance
+}
